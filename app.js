59b46bfc630bc44888659d2fdefb57b719f99804
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-11 11:57:24 -0300

    N47 - repetir menu forma infinita

--- app.js
+++ app.js
@@ -6,9 +6,18 @@ console.clear();
 
 const main = async() =>{
     console.log('Hola Mundo');
-    mostrarMenu();
+    
+    let opt = '';
+    
+    do{
+        opt = await mostrarMenu();
+        console.log({opt});
+        if ( opt !== '0') await pausa();
 
-    //pausa();
+
+    } while(opt !== '0')
+
+ 
 }
 
 main();
