79635cb0f7da4e9d83c46329e6657fca00a92e5b
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-14 14:04:45 -0300

    N52 - Crear y listar tareas

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import colors from 'colors'; 
-import { inquirerMenu, pausa } from './helpers/inquirer.js';
+import { inquirerMenu,
+         pausa,
+         leerInput } from './helpers/inquirer.js';
 //import { Tarea } from './models/tarea.js';
 import {Tareas} from './models/tareas.js';
  
@@ -12,11 +14,28 @@ const main = async() =>{
     console.log('Hola Mundo');
     
     let opt = '';
+    const tareas = new Tareas();
     
     do{
           opt = await inquirerMenu();
-          console.log({opt});
 
+        switch (opt) {
+            case '1':
+                // crear opcion
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea(desc)
+                
+            break;
+
+            case '2':
+                console.log(tareas._listado);
+                break;
+                
+        }
+
+
+
+          //console.log({opt});
         // const tareas = new Tareas();
         // const tarea = new Tarea('Comprar comida');
         // console.log(tareas);
